868a139e9eabfd7edc5fb89fd37932efdbf9b021
Shehan Chanuka <shehanxdev@gmail.com>
2023-01-25 00:37:29 +0530

    Configured to be deployed on Vercel.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  const [state, setState] = useState("");
+  useEffect(() => {
+    axios.get("http://localhost:3001/api").then((result) => {
+      setState(result.data.message);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{state}</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,1 +1,60 @@
-console.log("hi");
+/* MODULES */
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+const { fileURLToPath } = require("url");
+const terminal_kit = require("terminal-kit");
+
+/* CONFIGURATIONS */
+dotenv.config({ path: __dirname + "/.env" });
+const app = express();
+const terminal = terminal_kit.terminal;
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/* MONGODB CONFIGUARATION AND STARTING APP */
+const PORT = process.env.PORT;
+mongoose
+  .connect(process.env.MOnGODB_URL, {
+    usenewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Surge",
+  })
+  .then(() => {
+    terminal.bold.green("MongoDB is connected\n");
+    if (PORT) {
+      app.listen(PORT, () => {
+        console.log(__dirname);
+        terminal.blue.bold(`App is listening at the port ${PORT}`);
+      });
+    }
+  })
+  .catch((error) => {
+    terminal.bold.red(
+      "Did not connect to the database. ERROR: " + error + "\n"
+    );
+  });
+/* CONFIGUARING ROUTES */
+app.use("/api", (req, res) => {
+  res.json({ message: "Hi" });
+});
+
+// static files (build of the frontend)
+if (process.env.NODE_ENV == "PRODUCTION") {
+  app.use(express.static(path.join(__dirname + "../client", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  });
+}
+//*exporting app object to vercel
+// module.exports = app;
